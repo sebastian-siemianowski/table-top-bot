c0db52610ac7712c9dcfe25f1e76d81902fa8da2
sebastian.siemianowski <rudph2@gmail.com>
2025-05-22 03:12:01 +0100

    Adds additional functionality to table

--- lib/table.rb
+++ lib/table.rb
@@ -9,8 +9,10 @@ module TableTopBot
       @height = 5
     end
 
-    def valid_position?(width: , height:)
-      true
+    def valid_position?(width:, height:)
+      width.is_a?(Integer) && height.is_a?(Integer) &&
+        width >= 0 && width < @width &&
+        height >= 0 && height < @height
     end
   end
 end
--- spec/lib/table_spec.rb
+++ spec/lib/table_spec.rb
@@ -21,5 +21,17 @@ RSpec.describe TableTopBot::Table do
     it 'returns true for valid position' do
       expect(table.valid_position?(width: 0, height: 0)).to eq true
     end
+
+    it 'returns false for invalid position' do
+      expect(table.valid_position?(width: 5, height: 0)).to eq false
+    end
+
+    it 'returns false if width is not integer' do
+      expect(table.valid_position?(width: nil, height: 0)).to eq false
+    end
+
+    it 'returns false if height is not integer' do
+      expect(table.valid_position?(width: 0, height: nil)).to eq false
+    end
   end
 end
