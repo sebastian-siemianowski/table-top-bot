0287b1c32a0007575c3fb7b4a6b533e2d5cb5910
sebastian.siemianowski <rudph2@gmail.com>
2025-05-22 18:46:56 +0100

    Adds failing tests

--- spec/lib/direction_spec.rb
+++ spec/lib/direction_spec.rb
@@ -10,4 +10,38 @@ RSpec.describe TableTopBot::Direction do
       expect(described_class::DIRECTIONS).to eq %w[NORTH EAST SOUTH WEST]
     end
   end
+
+  describe '.valid?' do
+    it 'returns true for NORTH' do
+      expect(described_class.valid?('NORTH')).to be true
+    end
+
+    it 'returns true for EAST' do
+      expect(described_class.valid?('EAST')).to be true
+    end
+
+    it 'returns true for SOUTH' do
+      expect(described_class.valid?('SOUTH')).to be true
+    end
+
+    it 'returns true for WEST' do
+      expect(described_class.valid?('WEST')).to be true
+    end
+
+    it 'returns false for UP' do
+      expect(described_class.valid?('UP')).to be false
+    end
+
+    it 'returns false for DOWN' do
+      expect(described_class.valid?('DOWN')).to be false
+    end
+
+    it 'returns false for empty string' do
+      expect(described_class.valid?('')).to be false
+    end
+
+    it 'returns false for nil' do
+      expect(described_class.valid?(nil)).to be false
+    end
+  end
 end
